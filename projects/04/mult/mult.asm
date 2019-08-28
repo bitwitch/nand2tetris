758e3dfd83b19db1b9ae947f3af56144bf6c8f10
bitwitch <shawwian@gmail.com>
2019-08-28 01:05:15 -0400

    assembly program: multiply

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// set product to 0
+@R2
+M=0
+
+// result is 0 if R1 or R2 is 0
+@R0
+D=M
+@END
+D;JEQ
+@R1
+D=M
+@END
+D;JEQ
+
+// compute product
+(LOOP)
+@R0
+D=M
+@R2
+M=M+D
+@R1
+MD=M-1
+@END
+D;JLE
+@LOOP
+0;JMP
+(END)
