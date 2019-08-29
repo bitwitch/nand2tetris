1956d3e1f87b10cb6e8a11f98ad713fcecbc86ee
bitwitch <shawwian@gmail.com>
2019-08-29 00:19:22 -0400

    assembly program: fill screen on keydown

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,50 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(INPUT)
+@KBD
+D=M
+@BLACK
+D;JNE
+@WHITE
+0;JMP
+
+(BLACK)
+@SCREEN
+D=A
+(BLACKLOOP)
+A=D
+M=-1
+D=D+1
+// test if mem is at keyboard
+@R0
+M=D
+@KBD
+D=D-A
+@INPUT
+D;JEQ
+// else keep drawing
+@R0
+D=M
+@BLACKLOOP
+0;JMP
+
+(WHITE)
+@SCREEN
+D=A
+(WHITELOOP)
+A=D
+M=0
+D=D+1
+// test if mem is at keyboard
+@R0
+M=D
+@KBD
+D=D-A
+@INPUT
+D;JEQ
+// else keep drawing
+@R0
+D=M
+@WHITELOOP
+0;JMP
